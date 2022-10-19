6c377a119f8efdb6c46b6607bfae5d31a2eb1f3c
Chibuike Ilo <82292997+WESTIEWEB@users.noreply.github.com>
2022-10-19 02:56:09 +0100

    week one task- app.js refactored, model.js cleard

--- task-one/src/app.js
+++ task-one/src/app.js
@@ -1,13 +1,30 @@
-import FileTree from './model';
+import FileTree from './fileTree';
 
 export function createFileTree(input) {
+
+
+  // to not mutate the file
+  let tree = JSON.parse(JSON.stringify(input))
+
+  let treeInput = []; // this is an empty container to holde array of node without parentID, and array of nodes with parentID
+
+  //sorts  the tree array, to get arrays of node with/without parentID
+  tree.array.forEach(node => {
+    if(!node.id){
+      treeInput.push([node.id])
+    } else{
+      let tosort = [];
+      tosort.push(node.id);
+      treeInput.push(tosort.sort((a,b) => a-b))
+    }
+    
+  });
+
   const fileTree = new FileTree();
 
-  for (const inputNode of input) {
+  for (const inputNode of treeInput.flat(3)) {
     const parentNode = inputNode.parentId
-      ? fileTree.findNodeById(inputNode.parentId) === null
-      ?fileTree.createParentTree(inputNode)
-      :fileTree.findNodeById(inputNode.parentId)
+      ? fileTree.findNodeById(inputNode.parentId)
       : null;
 
     fileTree.createNode(
@@ -20,3 +37,4 @@ export function createFileTree(input) {
 
   return fileTree;
 }
+
